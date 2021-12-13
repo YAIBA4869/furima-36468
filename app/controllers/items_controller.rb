6101ca22201a860efef0020be9ab5296a9051db7
YAIBA4869 <daisuke.0104yuki@gmail.com>
2021-12-13 18:59:23 +0900

    商品詳細画面へ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:product_name, :product_description, :product_condition_id, :category_id, :price, :shipping_charges_id, :prefecture_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
