ef40ecc17ffd563f248d10e8e55554eec9a206dc
YAIBA4869 <daisuke.0104yuki@gmail.com>
2021-12-18 11:41:35 +0900

    商品情報編集ページ実装

--- db/migrate/20211115092755_create_items.rb
+++ db/migrate/20211115092755_create_items.rb
@@ -7,7 +7,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :product_condition_id, null: false
       t.integer :category_id, null: false
       t.integer :price, null: false
-      t.integer :shopping_charges_id, null: false
+      t.integer :shipping_charges_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :days_to_ship_id, null: false
 
