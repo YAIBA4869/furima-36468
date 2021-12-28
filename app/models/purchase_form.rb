ac2a2edf53fd8dff8df43dcc71af1f95f4d8701e
YAIBA4869 <daisuke.0104yuki@gmail.com>
2021-12-28 16:21:25 +0900

    最終修正

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,6 +1,6 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :purchase_record_id, :postal_code, :city, :prefecture_id, :addresses, :building, :phone_number, :user_id, :item_id, :token
+  attr_accessor :postal_code, :city, :prefecture_id, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-), Input half-width characters"}
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    # email                 {'test@test.com'}
     email                 {Faker::Internet.free_email}
     password              {'12d456'}
     password_confirmation {password}
